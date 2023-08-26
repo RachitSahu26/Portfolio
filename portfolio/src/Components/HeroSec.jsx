9b82f092dcb53f066748b9efdd47e0d9087e9f76
rachit <curiousrachit.26@gmail.com>
2023-08-26 21:45:26 +0530

    completed a nav section

--- portfolio/src/Components/HeroSec.jsx
+++ portfolio/src/Components/HeroSec.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import "../Components/HeroStyle.css"
+import videoSource from '../assets/snow.mp4';
 function HomeSec() {
     return (
-        <div className='hero-detail'>
+
+
+        <div className="hero">
+            {/* ...................added a video whole website............ */}
+            <div className="hero-video">
+
+                <video className='snow-video' muted>
+                    <source src={videoSource} type="video/mp4" />
+                </video>
+            </div>
+
+
+
+            {/* ...................user detail ................. */}
             <div className="user-contnet">
                 <h1> Hi, I am Rachit Sahu</h1>
                 <p>React<br />Developer</p>
@@ -11,9 +25,20 @@ function HomeSec() {
                     <button className='btn-light btn'> Detail</button>
 
                 </div>
+
             </div>
 
-        </div>
+
+
+
+
+
+
+
+
+
+
+         </div>
     )
 }
 
